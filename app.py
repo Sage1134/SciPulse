111f9158c6b3cf3b6650fea0e170311f3f726649
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2023-12-09 15:09:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from kivy.core.window import Window
 
 Window.size = (300, 500)
 
+user = ''
+
 layout_helper = '''
 Screen:
     BoxLayout:
@@ -25,20 +27,19 @@ MDTextField:
     width: 300
 """
 
-screen_helper = """
+homepage = """
 Screen:
     BoxLayout:
         orientation: 'vertical'
         MDTopAppBar:
             md_bg_color: 6/225, 204/225, 110/225, 1
-            title:'Biology Scanner'
+            title:'Homepage'
 
         MDLabel:
             id: welcome_label
             text_size: self.size
             halign: 'left'
             valign: 'top'
-            text: "Welcome"
             theme_text_color: "Custom"
             text_color: 50/225.0, 104/255.0, 168/255.0, 1
             font_style: 'H6'
@@ -51,7 +52,7 @@ class App(MDApp):
         self.theme_cls.primary_palette = "Green"
         self.dialog = None  # Define the dialog attribute
         button = MDRectangleFlatButton(text='Login', pos_hint={
-                                       'center_x': 0.5, 'center_y': 0.4}, on_release=self.switchToHome)
+                                       'center_x': 0.5, 'center_y': 0.4}, on_release=self.loginCheck)
         self.username = Builder.load_string(username_helper)
         layout = Builder.load_string(layout_helper)
         self.screen.add_widget(self.username)
@@ -59,28 +60,23 @@ class App(MDApp):
         self.screen.add_widget(button)
         return self.screen
 
-    def switchToHome(self, obj):
+    def loginCheck(self, obj):
         if self.username.text == "":
             check_string = 'Please enter a username'
             self.dialog = MDDialog(title='Username check',
                                    text=check_string,
                                    size_hint=(0.8, 1),
-                                   buttons=[MDFlatButton(text='Close', on_release=self.close_dialog)]
+                                   buttons=[MDFlatButton(
+                                       text='Close', on_release=self.close_dialog)]
                                    )
             self.dialog.open()
         else:
             self.screen.clear_widgets()
-            second_screen = Builder.load_string(screen_helper)
+            second_screen = Builder.load_string(homepage)
             self.screen.add_widget(second_screen)
             biology_button = MDRectangleFlatButton(text='Biology', pos=(
-                50, 50), on_release=self.switchToBiology)
+                50, 50))
             self.screen.add_widget(biology_button)
-
-    def switchToBiology(self, obj):
-        # Access the MDTextField's text attribute and update the label
-        username_text = self.screen.ids.username_field.text
-        welcome_label = self.screen.ids.welcome_label
-        welcome_label.text = f"Welcome {username_text}"
 
     def close_dialog(self, obj):
         self.dialog.dismiss()
