959cef9baf03413feb33e74f1948f7ee7aa29d65
2006ak <151586244+2006ak@users.noreply.github.com>
2023-12-09 14:51:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDRectangleFlatButton
 from kivy.core.window import Window
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+
 
 Window.size = (300, 500)
 
@@ -47,19 +50,32 @@ class Signin(MDApp):
         self.theme_cls.primary_palette = "Green"
         button = MDRectangleFlatButton(text='Login', pos_hint={
                                        'center_x': 0.5, 'center_y': 0.4}, on_release=self.switch_screen)
-        username = Builder.load_string(username_helper)
+        self.username = Builder.load_string(username_helper)
         layout = Builder.load_string(layout_helper)
-        self.screen.add_widget(username)
+        self.screen.add_widget(self.username)
         self.screen.add_widget(layout)
         self.screen.add_widget(button)
         return self.screen
 
     def switch_screen(self, obj):
-        # Switch to the second screen
-        self.screen.clear_widgets()
-        second_screen = Builder.load_string(screen_helper)
-        self.screen.add_widget(second_screen)
+        if self.username.text is "":
+            check_string = 'Please enter a username'
+            self.dialog = MDDialog(title='Username check',
+                       text=check_string,  # Change this line
+                       size_hint=(0.8, 1),
+                       buttons=[MDFlatButton(text='Close', on_release=self.close_dialog)]
+                       )
+            self.dialog.open()
+        else:
+            self.screen.clear_widgets()
+            second_screen = Builder.load_string(screen_helper)
+            self.screen.add_widget(second_screen)
 
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
+    
+        
 
 class MyApp(MDApp):
     def build(self):
